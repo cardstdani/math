2de12f269e7b0ad39ca7413b8992dd582f15c5e2
cardstdani <72665778+cardstdani@users.noreply.github.com>
2021-09-15 19:58:48 +0200

    Update Derivatives3D.py

--- Derivatives/Derivatives3D.py
+++ Derivatives/Derivatives3D.py
@@ -33,11 +33,6 @@ def plotLineY(m, n, r=30):
     return
 
 
-def plotGradient(m1, m2, x, y, r=20):
-    ax.plot([x - r, x + r], [y - r, y + r], [m2 * (x - r) + m1 * (y - r) + f(x, y), m2 * (x + r) + m1 * (y + r) + f(x, y)])
-    return
-
-
 res = 500
 x = np.linspace(-30, 30, res)
 y = np.linspace(-30, 30, res)
@@ -60,5 +55,4 @@ print(resultY)
 
 plotLineX(resultX[0], resultX[1])
 plotLineY(resultY[0], resultY[1])
-plotGradient(resultX[0], resultY[0], xValue, yValue)
 plt.show()
